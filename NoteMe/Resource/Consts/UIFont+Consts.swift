b00919f61246c0b654c8ee8be6fe23878eb7f01c
Pavel Krampuls <96665035+PavelKrm@users.noreply.github.com>
2023-10-24 20:52:11 +0300

    added snapKit, added constants

---
+++ NoteMe/Resource/Consts/UIFont+Consts.swift
@@ -0,0 +1,8 @@
+//
+//  UIFont+Consts.swift
+//  NoteMe
+//
+//  Created by PavelKrm on 24.10.23.
+//
+
+import Foundation
---
+++ NoteMe/Resource/Consts/UIImage+Consts.swift
@@ -0,0 +1,8 @@
+//
+//  UIImage+Consts.swift
+//  NoteMe
+//
+//  Created by PavelKrm on 24.10.23.
+//
+
+import Foundation
