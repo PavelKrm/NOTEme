5ed80d1d5d801f74bc24aa997517d0a8600aefb7
Pavel Krampuls <96665035+PavelKrm@users.noreply.github.com>
2023-10-31 20:06:57 +0300

    added UIButton+Styles

---
+++ NoteMe/Styles/UIButton+Styles.swift
@@ -0,0 +1,8 @@
+//
+//  UIButton+Styles.swift
+//  NoteMe
+//
+//  Created by PavelKrm on 31.10.23.
+//
+
+import Foundation
---
+++ NoteMe/Utils/View+Layer.swift
@@ -0,0 +1,8 @@
+//
+//  View+Layer.swift
+//  NoteMe
+//
+//  Created by PavelKrm on 31.10.23.
+//
+
+import Foundation
